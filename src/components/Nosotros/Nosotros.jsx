87d9356569e411a19b75dd9bcf9b6a97a98302be
AkumaxDDD.gaspar.ahumada101@gmail.com <AkumaxDDD>
2024-12-14 19:22:31 -0300

    responsive us1

--- src/components/Nosotros/Nosotros.jsx
+++ src/components/Nosotros/Nosotros.jsx
@@ -23,23 +23,27 @@ function Nosotros() {
   }, []);
 
   return (
-    <div className="nosotros-contenido" id="nosotros">
+
+    <div className="nosotros-contenido">
       <div className="titulo-nosotros">
         <h1 className="titulo">Nosotros</h1>
       </div>
-      <div className="nosotros-text">
-        <h3 className="text-nosotros">
-          StudioCinco es más que un gimnasio, es el corazón de nuestra casa.
-          Aquí, tanto madre como hijo comparten su pasión por el deporte, 
-          guiando a cada miembro con dedicación y experiencia. En este espacio familiar, 
-          te invitamos a ser parte de nuestra comunidad, donde el bienestar y el compañerismo 
-          son nuestro estilo de vida.
-        </h3>
-      </div>
-      <div className="frente-contenedor">
-        <div className="frente"></div>
+      <div className="nosotros"  id="nosotros">
+        <div className="nosotros-text">
+          <h3 className="text-nosotros">
+            StudioCinco es más que un gimnasio, es el corazón de nuestra casa.
+            Aquí, tanto madre como hijo comparten su pasión por el deporte, 
+            guiando a cada miembro con dedicación y experiencia. En este espacio familiar, 
+            te invitamos a ser parte de nuestra comunidad, donde el bienestar y el compañerismo 
+            son nuestro estilo de vida.
+          </h3>
+        </div>
+        <div className="frente-contenedor">
+          <div className="frente"></div>
+        </div>
       </div>
     </div>
+      
   );
 }
 
